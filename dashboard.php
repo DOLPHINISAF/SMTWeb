e1c07c375590f468266884d7a3b39ed422f0e021
DOLPHINISAF <catalin.nedelescu@yahoo.com>
2025-01-17 16:11:57 +0200

    added js script for getting real time data from the server

--- dashboard.php
+++ dashboard.php
@@ -6,7 +6,11 @@ if(empty($_SESSION['username'])){
     exit();
 }
 
+$ApiKey = '';
 
+if(isset($_SESSION['APIKEY'])){
+    $ApiKey = $_SESSION['APIKEY'];
+}
 ?>
 
 
@@ -23,19 +27,120 @@ if(empty($_SESSION['username'])){
     <title>Dashboard</title>
 </head>
 <body>
-    
         <div class="header">
             <div class="api-key">
-
-              <span>API Key: <?php echo $_SESSION['APIKEY']; ?></span>
+              <span>API Key: <?php echo $ApiKey; ?></span>
             </div>
-            <div style="flex: 3;text-align: center;">
-                This is your Dashboard
+            <div class="dashboard-title">
+                Dashboard
             </div>
-            <div class="logout-button">
-               <a href="logout.php" class="logout-button">Log out</button>
+            <div class="logout">
+               <a href="logout.php" class="logout-button">Log out</a>
             </div>
         </div>
-    
+        
+        <div class="interact-panel">
+            <div class="option-panel">
+                <div class="option-buttons" id="monitor-button" onclick="GetMonitorData(this)">
+                    Monitor
+                </div>
+                <div class="option-buttons"  id="action-button" onclick="GetActions(this)">
+                    Actions
+                </div>
+            </div>
+               
+            <div>
+                <table id="live-data-table">
+                    <tr class="table-rows">
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Value</th>
+                        <th>Status</th>
+                    </tr>
+
+                    
+                </table>
+                <table id="action-table">
+                    <tr class="table-rows">
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th><button>
+                        </button></th>
+                    </tr>
+
+                    
+                </table>
+            </div>
+        </div>
+
+        <script>
+
+                UNSELECTED_BGCOLOR = "red";
+                SELECTED_BGCOLOR = "white"
+                function GetMonitorData(elem){
+                    document.getElementById("action-button").style.backgroundColor = UNSELECTED_BGCOLOR;
+                    elem.style.backgroundColor  = SELECTED_BGCOLOR;
+                    document.getElementById("live-data-table").style.display = "block";
+                    document.getElementById("action-table").style.display = "none";
+                    
+                }
+                function GetActions(elem){
+                    document.getElementById("monitor-button").style.backgroundColor = UNSELECTED_BGCOLOR;
+                    elem.style.backgroundColor  = SELECTED_BGCOLOR;
+                    document.getElementById("live-data-table").style.display = "none";
+                    document.getElementById("action-table").style.display = "block";
+                    
+                }
+
+                //we call it to default the selection
+                GetMonitorData(document.getElementById("monitor-button"));
+                
+
+                const ws = new WebSocket('ws://localhost:1337');
+
+                ws.addEventListener("open", () =>{
+                    APIKEY = `<?php echo $ApiKey ?>`;
+                    authdata = `{
+                                    "type":"auth",
+                                    "APIKey": "${APIKEY}"
+                                }`;
+                    ws.send(authdata);
+                })
+                ws.addEventListener("message", (message)=>{
+                    msgjson = JSON.parse(message.data);
+
+                    if(msgjson.type){
+                        if(msgjson.type === "data"){
+                            dataID = msgjson.dataID;
+                            dataElement = document.getElementById(`.${dataID}`);
+
+                            document.getElementById(`${dataID}-value`).innerHTML = msgjson.value;
+                            if(msgjson.status){
+                                document.getElementById(`${dataID}-status`).innerHTML = msgjson.status;
+                            }
+                        }
+                        else if(msgjson.type === "add"){
+
+                            let dataID = msgjson.dataID;
+                            let description = msgjson.description;
+                            let status = "OK";
+                            let value = "NULL";
+
+                            document.getElementById("live-data-table").innerHTML=
+                                `<tr id="${dataID}" class="table-rows">
+                                    <th>${dataID}</th>
+                                    <th>${description}</th>
+                                    <th id = "${dataID}-value">${value}</th>
+                                    <th id = "${dataID}-status">${status}</th>
+                                </tr>`;
+                        
+                        }
+                        
+                   }
+                });
+
+
+            </script>
+             
 </body>
 </html>
