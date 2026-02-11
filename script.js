3f7515b4becd2fce4a0ddebebb88728a410a2094
DOLPHINISAF <catalin.nedelescu@yahoo.com>
2026-02-11 21:16:58 +0200

    small changes

--- script.js
+++ script.js
@@ -32,44 +32,47 @@ GetMonitorData(document.getElementById("monitor-button"));
 
 const ws = new WebSocket('ws://localhost:1337');
 
+//used to announce server that user is online and can receive data
 ws.addEventListener("open", () =>{
+    //we get the api key
     APIKEY = `<?php echo $ApiKey ?>`;
-    authdata = `{
-                    "type":"auth",
-                    "APIKey": "${APIKEY}"
-                }`;
-    ws.send(authdata);
+
+    //we make the string json with needed data
+        authdata = {
+        type:"auth",
+        APIKey:APIKEY,
+        source:"client"
+    }
+    ws.send(JSON.stringify(authdata));
 })
+
 ws.addEventListener("message", (message)=>{
     msgjson = JSON.parse(message.data);
 
     if(msgjson.type){
         if(msgjson.type === "data"){
-            dataID = msgjson.dataID;
-            dataElement = document.getElementById(`.${dataID}`);
+            nameID = msgjson.nameID;
+            dataElement = document.getElementById(`.${nameID}`);
 
-            document.getElementById(`${dataID}-value`).innerHTML = msgjson.value;
-            if(msgjson.status){
-                document.getElementById(`${dataID}-status`).innerHTML = msgjson.status;
-            }
+            document.getElementById(`${nameID}-value`).innerHTML = msgjson.value;
         }
         else if(msgjson.type === "add"){
 
-            let dataID = msgjson.dataID;
+            let nameID = msgjson.nameID;
             let description = msgjson.description;
-            let status = "OK";
+            let unit = msgjson.unit;
             let value = "NULL";
 
             document.getElementById("live-data-table").innerHTML=
-                `<tr id="${dataID}" class="table-rows">
-                    <th>${dataID}</th>
+                `<tr id="${nameID}" class="table-rows">
+                    <th>${nameID}</th>
                     <th>${description}</th>
-                    <th id = "${dataID}-value">${value}</th>
-                    <th id = "${dataID}-status">${status}</th>
+                    <th id = "${nameID}-value">${value}</th>
+                    <th>${unit}</th>
                 </tr>`;
         
         }
         
-   }
+    }
 });
 
