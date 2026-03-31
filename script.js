c204cf2baa07f22bdc0dcb1cc74358f71f873f1f
DOLPHINISAF <catalin.nedelescu@yahoo.com>
2026-03-31 11:55:14 +0300

    Implemented logic for running user defined action

--- script.js
+++ script.js
@@ -16,34 +16,42 @@ function GetActions(elem){
     
 }
 
-function ActivateAction(action_name){
+function ActivateAction(actionID){
 
     msgjson = {
-        type:"action",
-        actionID:action_name
+        type:"run_action",
+        actionID:actionID,
+        APIKey: API_KEY
     }
 
+    try{
     ws.send(JSON.stringify(msgjson));
+        console.log(`Sent ActivateAction to server, action: ${actionID}`)
+    }
+    catch (error) {
+        console.log(`Failed to use websocket to send json.\nError: ${error}`)
+    }
+    
 }
 
 //we call it to default the selection
 GetMonitorData(document.getElementById("monitor-button"));
 
-
-const ws = new WebSocket('ws://localhost:1337');
+//websocket path to backend server
+const ws = new WebSocket('ws://dolphinsibiu.ddns.net:1337');
 
 //used to announce server that user is online and can receive data
 ws.addEventListener("open", () =>{
-    //we get the api key
-    APIKEY = `<?php echo $ApiKey ?>`;
-
+    console.log(API_KEY)
     //we make the string json with needed data
         authdata = {
         type:"auth",
-        APIKey:APIKEY,
+        APIKey:API_KEY,
         source:"client"
     }
     ws.send(JSON.stringify(authdata));
+
+    console.log("Connected websocket to backend server.")
 })
 
 ws.addEventListener("message", (message)=>{
