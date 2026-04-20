c5c39a76cc83f211544d986259c4ac0fcef13d6d
DOLPHINISAF <catalin.nedelescu@yahoo.com>
2026-04-20 17:14:18 +0300

    V0.1.0:
    Changes:
    -now adding version numbering to commits, currently api and backend server should have same version to prevent any problems
    -added UI for creating and removing new parameters, actions. Done to remove uncomfortable to use methods inside the api, all is done from web ui now.
    -minor UI changes
    -and also added script logic for getting/modifying user config from db
    -small bug fixes

--- dashboard.php
+++ dashboard.php
@@ -64,9 +64,17 @@ if(isset($_SESSION['APIKEY'])){
                         <th class="description-column">Description</th>
                         <th class="value-column">Value</th>
                         <th class="unit-column">Unit</th>
+                        <th class="edit-column">Edit</th>
                     </thead>
 
                     <tbody id="live-data-table-body">
+                        <tr id="dataInsertRow" class="table-rows">
+                            <td class="name-column"><input id ="parameter-name-input" placeholder="Name" class = "placeholder"></td>
+                            <td class="description-column"><input id ="parameter-description-input" placeholder="Description" class = "placeholder"></td>
+                            <td class="value-column"></td>
+                            <td class="unit-column"><input id ="parameter-unit-input" placeholder="Unit" class = "placeholder"></td>
+                            <td class="edit-column"><button class = "action-run-button"onclick="SaveUserItem('parameter')">Add parameter</button></td>
+                        </tr>
                     </tbody>
                     
                 </table>
@@ -74,10 +82,19 @@ if(isset($_SESSION['APIKEY'])){
                     <thead class="table-rows">
                         <th class="name-column">Name</th>
                         <th class="description-column">Description</th>
+                        <th class="action-status-column">Status</th>
                         <th class="action-button-column">Activate</th>
+                        <th class="edit-column">Edit</th>
                     </thead>
 
                     <tbody id="action-table-body">
+                        <tr id="actionInsertRow" class="table-rows">
+                            <td class="name-column"><input id ="action-name-input" placeholder="Action Name" class = "placeholder"></td>
+                            <td class="description-column"><input id ="action-description-input" placeholder="Description" class = "placeholder"></td>
+                            <td class="action-status-column"></td>
+                            <td class="action-button-column"></td>
+                            <td class="edit-column"><button class = "action-run-button"  onclick="SaveUserItem('action')">Add action</button></td>
+                        </tr>
                     </tbody>
 
                     
